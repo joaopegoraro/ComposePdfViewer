97f8290d7a9f5b09f15728960bbaf3006a7a0a92
João Pegoraro <hahmraro@tuta.io>
2022-03-09 13:19:29 -0300

    Added demo code and app

--- app/src/main/java/xyz/joaophp/composepdfviewer/MainActivity.kt
+++ app/src/main/java/xyz/joaophp/composepdfviewer/MainActivity.kt
@@ -3,12 +3,16 @@ package xyz.joaophp.composepdfviewer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import xyz.joaophp.composepdfviewer.ui.theme.ComposePdfViewerTheme
+import xyz.joaophp.pdfviewer.PdfViewer
 
+@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,14 +20,18 @@ class MainActivity : ComponentActivity() {
             ComposePdfViewerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    DemoLayout()
                 }
             }
         }
     }
 }
 
+@ExperimentalFoundationApi
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DemoLayout() {
+    PdfViewer(
+        modifier = Modifier.fillMaxSize(),
+        pdfResId = R.raw.demo
+    )
 }
